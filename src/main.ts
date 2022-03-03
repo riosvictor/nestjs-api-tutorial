cf3d4a144a04cb9c0d155008e2e83f55ccd662cb
Paulo Victor Rios <victorios.ba@gmail.com>
2022-03-03 15:51:49 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  HttpStatus,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  const port = 3333;
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      errorHttpStatusCode:
+        HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
 
-  await app.listen(3333);
+  await app.listen(port);
+
+  logger.log(
+    `Application listing on port ${port}`,
+  );
 }
 bootstrap();
